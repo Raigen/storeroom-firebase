86fcac9be687672b152620b2905443e8eb87bc85
Florian Oellerich <falkentavio@hotmail.de>
2019-06-23 10:23:38 +0200

    feat: add first real test
    Add react testing library.
    Add test for navigation component.

--- src/components/__tests__/Navigation.spec.tsx
+++ src/components/__tests__/Navigation.spec.tsx
@@ -1,3 +1,26 @@
-it('dummy test', function() {
-  expect(true).toBeTruthy()
+import { Navigation } from '../Navigation'
+import React from 'react'
+import { render } from '@testing-library/react'
+
+jest.mock('../rooms/RoomList', () => ({
+  RoomsList: () => <div data-testid="room-list" />
+}))
+
+jest.mock('../firebase/hooks', () => ({
+  useFirebaseUser: jest
+    .fn()
+    .mockReturnValueOnce(null)
+    .mockReturnValueOnce({ uid: '1' })
+}))
+
+it('renders nothing when not logged in', function() {
+  const { queryByTestId } = render(<Navigation drawerWidth={100} />)
+
+  expect(queryByTestId('room-list')).toBeNull()
+})
+
+it('renders the roomlist for logged in users', function() {
+  const { queryByTestId } = render(<Navigation drawerWidth={100} />)
+
+  expect(queryByTestId('room-list')).toBeTruthy()
 })
