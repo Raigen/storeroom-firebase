35dbd785b4046814225cd89cbf34a25f9aa830fd
Florian Oellerich <falkentavio@hotmail.de>
2019-06-23 12:48:05 +0200

    chore: ignore some files in coverage

--- src/components/firebase/context.ts
+++ src/components/firebase/context.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import React from 'react'
 
 export const FirebaseContext = React.createContext(null)
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import './index.css'
 import './components/firebase/firebase.ts'
 
