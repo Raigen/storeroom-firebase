e113cdec3d8a1eac87cf2e975a9b00863a53574c
Florian Oellerich <falkentavio@hotmail.de>
2019-06-30 18:30:09 +0200

    freat: add new goods

--- src/components/goods/GoodList.tsx
+++ src/components/goods/GoodList.tsx
@@ -1,11 +1,33 @@
 import { GoodListEntry, GoodType } from './Good'
 
+import Button from '@material-ui/core/Button'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
 import { firestore } from '../firebase/firebase'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+const GoodInput: React.FC = () => {
+  const [name, setName] = React.useState('')
+  const [unit, setUnit] = React.useState('')
+
+  const saveNewEntry = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    await firestore.collection('goods/').add({ name, unit })
+    setName('')
+    setUnit('')
+  }
+
+  return (
+    <form onSubmit={saveNewEntry} data-testid="goodForm">
+      <TextField required value={name} onChange={event => setName(event.target.value)} placeholder="Zucker" />
+      <TextField required value={unit} onChange={event => setUnit(event.target.value)} placeholder="kg" />
+      <Button type="submit">Speichern</Button>
+    </form>
+  )
+}
 
 type ListProps = {}
 
@@ -22,6 +44,9 @@ export const GoodList: React.FC<ListProps> = () => {
           <ListItemText primary={<GoodListEntry good={good} />} />
         </ListItem>
       ))}
+      <ListItem dense>
+        <GoodInput />
+      </ListItem>
     </List>
   )
 }
