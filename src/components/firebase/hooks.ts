7e8278da7689d9f96226f91c99d5809ffa294a0e
Florian Oellerich <falkentavio@hotmail.de>
2019-11-22 19:58:44 +0100

    chore: simplify firebase hooks
    Use react-firebase-hooks in own hooks.

--- src/components/firebase/hooks.ts
+++ src/components/firebase/hooks.ts
@@ -1,4 +1,5 @@
 import { analytics, auth, firestore } from './firebase'
+import { useCollectionData, useDocumentDataOnce } from 'react-firebase-hooks/firestore'
 
 import React from 'react'
 import { RoomType } from '../rooms/Room'
@@ -13,20 +14,10 @@ type UserData = {
 export function useFirebaseUser(): { userData: UserData | null; user: firebase.User | null } {
   const router = React.useContext(__RouterContext)
   const [user, setUser] = React.useState<firebase.User | null>(null)
-  const [userData, setUserData] = React.useState<UserData | null>(null)
-  React.useEffect(() => {
-    if (user) {
-      firestore
-        .doc(`users/${user.uid}`)
-        .get()
-        .then(userData => {
-          const data = userData.data() as UserData
-          setUserData(data)
-        })
-    } else {
-      setUserData(null)
-    }
-  }, [user])
+
+  const docRef = user ? firestore.doc(`users/${user.uid}`) : null
+  const [userData = null, , error] = useDocumentDataOnce<UserData>(docRef)
+  if (error) console.error(error)
   React.useEffect(() => {
     const unregisterAuthObserver = auth.onAuthStateChanged(user => {
       setUser(user)
@@ -34,7 +25,6 @@ export function useFirebaseUser(): { userData: UserData | null; user: firebase.U
         router.history.push(SIGN_IN)
       } else {
         analytics.setUserId(user.uid)
-        // router.history.push(ROOMLIST)
       }
     })
     return () => unregisterAuthObserver()
@@ -50,31 +40,12 @@ type Household = {
 }
 
 export function useHousehold(): { household: Household | null; rooms: RoomType[] } {
-  const { user, userData } = useFirebaseUser()
-  const [household, setHousehold] = React.useState<Household | null>(null)
-  const [rooms, setRooms] = React.useState<RoomType[]>([])
-  React.useEffect(() => {
-    if (userData && user) {
-      firestore
-        .doc(`households/${userData.activeHousehold}`)
-        .get()
-        .then(snapshot => {
-          if (snapshot.exists) {
-            const data = { ...snapshot.data(), id: snapshot.ref.id } as Household
-            if (data && data.users.includes(user.uid)) setHousehold(data)
-          }
-        })
-    }
-  }, [userData, user])
-  React.useEffect(() => {
-    if (household) {
-      firestore
-        .collection(`households/${household.id}/rooms`)
-        .get()
-        .then(snapshot => {
-          setRooms(snapshot.docs.map(doc => ({ name: doc.get('name'), _id: doc.id } as RoomType)))
-        })
-    }
-  }, [household])
+  const { userData } = useFirebaseUser()
+
+  const householdRef = userData ? firestore.doc(`households/${userData.activeHousehold}`) : null
+  const [household = null] = useDocumentDataOnce<Household>(householdRef, { idField: 'id' })
+
+  const roomRef = household ? firestore.collection(`households/${household.id}/rooms`) : null
+  const [rooms = []] = useCollectionData<RoomType>(roomRef, { idField: '_id' })
   return { household, rooms }
 }
